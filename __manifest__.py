683a905d8de3d0553d56871950cbf6b506ae2a84
Tony Galmiche <tony.galmiche@infosaone.com>
2026-03-27 18:20:50 +0100

    Adaptation pour Odoo 18 + ajout README.md

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'InfoSaône - Module Odoo 19 pour ajouter le prénom dans le contact',
-    'version': '19.0.0.0.1',
+    'name': 'InfoSaône - Module Odoo 18 pour ajouter le prénom dans le contact',
+    'version': '18.0.0.0.1',
     'category': 'InfoSaône',
     'summary': 'Ajouter le prénom dans le contact',
     'description': """
-InfoSaône - Module Odoo 19 pour ajouter le prénom dans le contact
+InfoSaône - Module Odoo 18 pour ajouter le prénom dans le contact
 ===================================================
 """,
     'website': 'http://www.infosaone.com',
