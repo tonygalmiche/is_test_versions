07d0bd56646c5a3bb6c2566a8b1658d27e24a728
Tony Galmiche <tony.galmiche@infosaone.com>
2026-03-27 18:13:01 +0100

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'InfoSaône - Module Odoo 19 pour ajouter le prénom dans le contact',
+    'version': '19.0.0.0.1',
+    'category': 'InfoSaône',
+    'summary': 'Ajouter le prénom dans le contact',
+    'description': """
+InfoSaône - Module Odoo 19 pour ajouter le prénom dans le contact
+===================================================
+""",
+    'website': 'http://www.infosaone.com',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'author': 'InfoSaône',
+    'license': 'LGPL-3',
+}
