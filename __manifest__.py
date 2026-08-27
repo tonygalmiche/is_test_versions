3b88295c26ceb557989518e425af4ce5a9c2f354
Tony Galmiche <tony.galmiche@infosaone.com>
2026-08-27 14:34:01 +0200

    LICENSE

--- __manifest__.py
+++ __manifest__.py
@@ -17,5 +17,5 @@ InfoSaône - Module Odoo 18 pour ajouter le prénom dans le contact
     'installable': True,
     'application': True,
     'author': 'Tony Galmiche / InfoSaône',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
 }
